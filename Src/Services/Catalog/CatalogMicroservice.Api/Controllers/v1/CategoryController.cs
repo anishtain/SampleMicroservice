62c6ffc2f6a648aa65122b53622aa52f996eac04
anishtain <bagheri.mohammadreza10@gmail.com>
2022-03-25 12:31:28 +0430

    add controller to category service

---
+++ Src/Services/Catalog/CatalogMicroservice.Api/Controllers/v1/CategoryController.cs
@@ -0,0 +1,54 @@
+﻿using CatalogMicroservice.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogMicroservice.Api.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet("GetAllProducts")]
+        public async Task<IActionResult> GetAllProducts() =>
+            Ok(await _categoryRepository.GetProducts());
+
+        [HttpGet("GetAllProductsByName")]
+        public async Task<IActionResult> GetProductByName([FromQuery] string name) =>
+            Ok(await _categoryRepository.GetProductsByName(name));
+
+        [HttpGet("GetAllProductByCategory")]
+        public async Task<IActionResult> GetProductByCategory([FromQuery] string category) =>
+            Ok(await _categoryRepository.GetProductsByCategory(category));
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([FromBody] Entities.ProductEntity entity)
+        {
+            await _categoryRepository.CreateProduct(entity);
+
+            return Ok();
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> Update([FromBody] Entities.ProductEntity entity)
+        {
+            var result = await _categoryRepository.UpdateProduct(entity);
+
+            return Ok(result);
+        }
+
+        [HttpPost("Delete")]
+        public async Task<IActionResult> Delete([FromQuery] string id)
+        {
+            var result = await _categoryRepository.DeleteProduct(id);
+
+            return Ok(result);
+        }
+    }
+}
--- Src/Services/Catalog/CatalogMicroservice.Api/Program.cs
+++ Src/Services/Catalog/CatalogMicroservice.Api/Program.cs
@@ -1,3 +1,6 @@
+using CatalogMicroservice.Api.Data;
+using CatalogMicroservice.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICatalogDBContext, CatalogDbContext>();
 
 var app = builder.Build();
 
